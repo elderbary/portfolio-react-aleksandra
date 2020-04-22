5520f97bd038a4fad1450db488e75948050572d6
Dawid Rozanski <drozanski@outlook.com>
2020-04-22 21:55:47 +0200

    statsview komponent

---
+++ src/layouts/StatsView.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/stats.css';
+
+const StatsView = () => {
+    return (
+
+        <div className="flex-container-text1">
+            <h2>3453
+            </h2>
+            <p>
+                słów
+            </p>
+
+        </div>
+
+    )
+}
+
+export default StatsView; 
