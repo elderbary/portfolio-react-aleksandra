55d012594d582b38499ea7fbe51bbe5d752eaaad
Dawid Rozanski <drozanski@outlook.com>
2020-04-22 21:03:00 +0200

    dodane ikony socialowe

--- src/layouts/Social.js
+++ src/layouts/Social.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import '../styles/social.css';
+import facebook from '../images/facebook.png';
+import twitter from '../images/twitter.png';
+import instagram from '../images/instagram.png';
+
 const Social = () => {
     return (
-        <div> Social</div>
-    );
+        <div className="socialpic">
+            <ul>
+                <li>
+                    <img src={facebook} alt="facebook" />
+                </li>
+                <li>
+                    <img src={twitter} alt="twitter" />
+                </li>
+                <li>
+                    <img src={instagram} alt="instagram" />
+                </li>
+            </ul>
+        </div>
+    )
 }
 
 export default Social; 
