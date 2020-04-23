bc761e21d8c382eae1f0256c35d8d52a7e45e12e
Dawid Rozanski <drozanski@outlook.com>
2020-04-23 21:05:25 +0200

    walka z ramką

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -14,30 +14,32 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div className='container'>
-          <div className="grid">
-            <div className="grid-item grid-item-1">
-              {<Toggle />}
-            </div>
-            <div className="grid-item grid-item-2">
-              {<Text />}
-            </div>
-            <div className="grid-item grid-item-stats-1">
-              {<StatsWords />}
-            </div>
+        <div className='rama'>
+          <div className='container'>
+            <div className="grid">
+              <div className="grid-item grid-item-1">
+                {<Toggle />}
+              </div>
+              <div className="grid-item grid-item-2">
+                {<Text />}
+              </div>
+              <div className="grid-item grid-item-stats-1">
+                {<StatsWords />}
+              </div>
 
-            <div className="grid-item grid-item-stats-2">
-              {<StatsView />}
-            </div>
-            <div className="grid-item grid-item-3">
-              {<Picture />}
-              {<Social />}
-            </div>
-            <div className="grid-item grid-item-5">
-              {<Navigation />}
-            </div>
-            <div className="grid-item grid-item-6">
-              {<Butt />}
+              <div className="grid-item grid-item-stats-2">
+                {<StatsView />}
+              </div>
+              <div className="grid-item grid-item-3">
+                {<Picture />}
+                {<Social />}
+              </div>
+              <div className="grid-item grid-item-5">
+                {<Navigation />}
+              </div>
+              <div className="grid-item grid-item-6">
+                {<Butt />}
+              </div>
             </div>
           </div>
         </div>
