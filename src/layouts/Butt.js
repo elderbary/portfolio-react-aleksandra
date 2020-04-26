9adbe017edeb45c5610d6e14d473cf4ca3649097
Dawid Rozanski <drozanski@outlook.com>
2020-04-26 20:14:38 +0200

    asd

--- src/layouts/Butt.js
+++ src/layouts/Butt.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/butt.css';
 const Butt = () => {
     return (
-
         <div className="container">
             <button className="button" type="button">Zobacz więcej</button>
         </div>
