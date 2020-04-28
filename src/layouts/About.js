1333e6532fb0281ead1ffeb9b9b713b810d6d9bc
Dawid Rozanski <drozanski@outlook.com>
2020-04-28 22:00:11 +0200

    .

--- src/layouts/About.js
+++ src/layouts/About.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
 import "../styles/about.css";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -38,62 +37,6 @@ class About extends Component {
       </Router>
     );
   }
-=======
-import React, { Component } from 'react';
-import '../styles/about.css';
-import { BrowserRouter as Router } from 'react-router-dom'
-import Navigation from './Navigation';
-import Toggle from './Toggle';
-import Social from './Social';
-import Text from './Text';
-import Picture from './Picture';
-import Butt from './Butt';
-import StatsWordsCV from './StatsWordsCV';
-import StatsArticles from './StatsArticles';
-import StatsClients from './StatsClients';
-import StatsExp from './StatsExp';
-import Stats from './Stats';
+}
 
-class About extends Component {
-    render() {
-        return (
-            <Router>
-                <div className='rama'>
-                    <div className='container'>
-                        <div className="grid">
-                            <div className="grid-item grid-item-1">
-                                {<Toggle />}
-                            </div>
-                            <div className="grid-item grid-item-2">
-                                {<Text />}
-                            </div>
-                            <div className="grid-item experience">
-                                {<Stats upperText="test" bottomText="test" />}
-                            </div>
-                            <div className="grid-item clients">
-                                {<StatsClients />}
-                            </div>
-                            <div className="grid-item articles">
-                                {<StatsArticles />}
-                            </div>
-                            <div className="grid-item words">
-                                {<StatsWordsCV />}
-                            </div>
-                            <div className="grid-item grid-item-3">
-                                {<Picture />}
-                                {<Social />}
-                            </div>
-                            <div className="grid-item grid-item-5">
-                                {<Navigation />}
-                            </div>
-                            <div className="grid-item grid-item-6">
-                                {<Butt />}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </Router >
-        );
-    }
->>>>>>> d4fe6c12e66f805d6939ae7436d2040a8f9dc6c4}
 export default About;
--- src/layouts/Stats.js
+++ src/layouts/Stats.js
@@ -1,20 +1,15 @@
-import React, {Component} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/stats.css';
+import React, { Component } from "react";
+import "../styles/stats.css";
 
 class Stats extends Component {
-
-    render() {
-        return (
-            <div className="flex-container-StatsExp">
-                <h2>{this.props.upperText}
-                </h2>
-                <p>
-                    {this.props.bottomText}
-                </p>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="flex-container-StatsExp">
+        <h2>{this.props.upperText}</h2>
+        <p>{this.props.bottomText}</p>
+      </div>
+    );
+  }
 }
 
 export default Stats;
