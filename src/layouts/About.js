321e5cc01f984d6da769a68638e509ae79d11d86
Dawid Rozanski <drozanski@outlook.com>
2020-04-29 20:03:17 +0200

    .

--- src/layouts/About.js
+++ src/layouts/About.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import "../styles/about.css";
-import { BrowserRouter as Router } from "react-router-dom";
 import Navigation from "./Navigation";
 import Toggle from "./Toggle";
 import Social from "./Social";
@@ -15,22 +14,20 @@ import StatsExp from "./StatsExp";
 class About extends Component {
   render() {
     return (
-      <div className="rama">
-        <div className="container">
-          <div className="grid">
-            <div className="grid-item grid-item-1">{<Toggle />}</div>
-            <div className="grid-item grid-item-2">{<Text />}</div>
-            <div className="grid-item experience">{<StatsExp />}</div>
-            <div className="grid-item clients">{<StatsClients />}</div>
-            <div className="grid-item articles">{<StatsArticles />}</div>
-            <div className="grid-item words">{<StatsWordsCV />}</div>
-            <div className="grid-item grid-item-3">
-              {<Picture />}
-              {<Social />}
-            </div>
-            <div className="grid-item grid-item-5">{<Navigation />}</div>
-            <div className="grid-item grid-item-6">{<Butt />}</div>
+      <div className="container">
+        <div className="grid">
+          <div className="grid-item grid-item-1">{<Toggle />}</div>
+          <div className="grid-item grid-item-2">{<Text />}</div>
+          <div className="grid-item experience">{<StatsExp />}</div>
+          <div className="grid-item clients">{<StatsClients />}</div>
+          <div className="grid-item articles">{<StatsArticles />}</div>
+          <div className="grid-item words">{<StatsWordsCV />}</div>
+          <div className="grid-item grid-item-3">
+            {<Picture />}
+            {<Social />}
           </div>
+          <div className="grid-item grid-item-5">{<Navigation />}</div>
+          <div className="grid-item grid-item-6">{<Butt />}</div>
         </div>
       </div>
     );
