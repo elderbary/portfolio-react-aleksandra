f566ef72ea6872a5c34617991079db4b5d998075
Adam Kwiatek <salioner.adam@gmail.com>
2020-05-01 18:52:25 +0200

    fixes

--- src/layouts/Test.js
+++ src/layouts/Test.js
@@ -14,19 +14,32 @@ import StatsWordsCV from "./StatsWordsCV";
 import StatsArticles from "./StatsArticles";
 import StatsClients from "./StatsClients";
 import StatsExp from "./StatsExp";
+import Statistics from "./Statistics";
 
 class Test extends Component {
     render() {
         return (
             <Container className="container-std">
                 <Row className="row-main">
-                    <Col className="col-std col-toogle" md={1}>Toggle</Col>
-                    <Col className="col-std col-left" md={5}>Lewo</Col>
+                    <Col className="col-std col-toogle" md={1}>{<Toggle />}</Col>
+                    <Col className="col-std col-left" md={5}>
+                        <Container className="container-left">
+                            <Row className="row-text">
+                                <Col className="col-std col-text" md={12}>Test</Col>
+                            </Row>
+                            <Row className="row-stats">
+                                <Col className="col-std col-stats"><Statistics score="Test" description ="Test"/></Col>
+                                <Col className="col-std col-stats"><Statistics score="Test" description ="Test"/></Col>
+                                <Col className="col-std col-stats"><Statistics score="Test" description ="Test"/></Col>
+                                <Col className="col-std col-stats"><Statistics score="Test" description ="Test"/></Col>
+                            </Row>
+                        </Container>
+                    </Col>
                     <Col className="col-std col-right" md={5}>Prawo</Col>
-                    <Col className="col-std col-menu" md={1}>Menu</Col>
+                    <Col className="col-std col-nav" md={1}>{<Navigation />}</Col>
                 </Row>
                 <Row className="row-bottom">
-                    <Col className="col-std col-more" md={{ span: 2, offset: 5}}>See more</Col>
+                    <Col className="col-std col-btn" md={{ span: 2, offset: 5}}>{<Butt />}</Col>
                 </Row>
             </Container>
         );
