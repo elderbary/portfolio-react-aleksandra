507d95694ad1f2e4c3acbad157d192427ca5e29d
Dawid Rozanski <drozanski@outlook.com>
2020-05-02 22:43:39 +0200

    a

--- src/layouts/Social.js
+++ src/layouts/Social.js
@@ -6,7 +6,7 @@ import instagram from '../images/instagram.png';
 
 const Social = () => {
     return (
-        <div className="flex-container">
+        <div className="social-container">
             <ul>
                 <li>
                     <img src={facebook} alt="facebook" />
