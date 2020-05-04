6719dd8019a315ff02a46d104cf4d9293591863c
Dawid Rozanski <drozanski@outlook.com>
2020-05-04 21:46:36 +0200

    Updates

---
+++ precache-manifest.932c2c7c98571d7741354dbb5efd17c8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "123fc1848f0638fe1713dc79ee577a20",
+    "url": "/portfolio-react-aleksandra/index.html"
+  },
+  {
+    "revision": "3be9823e81c422083960",
+    "url": "/portfolio-react-aleksandra/static/css/2.a5b4ddd2.chunk.css"
+  },
+  {
+    "revision": "84378ddd2b46e0ea267f",
+    "url": "/portfolio-react-aleksandra/static/css/main.223e9c18.chunk.css"
+  },
+  {
+    "revision": "3be9823e81c422083960",
+    "url": "/portfolio-react-aleksandra/static/js/2.a376cf33.chunk.js"
+  },
+  {
+    "revision": "276f577d34079117de55ca81f2bf85d3",
+    "url": "/portfolio-react-aleksandra/static/js/2.a376cf33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84378ddd2b46e0ea267f",
+    "url": "/portfolio-react-aleksandra/static/js/main.cc8c632c.chunk.js"
+  },
+  {
+    "revision": "369edb8da53a46de4bd9",
+    "url": "/portfolio-react-aleksandra/static/js/runtime-main.cd21b5db.js"
+  },
+  {
+    "revision": "e4fdf3a74239d4645ba1d2c3b970acf2",
+    "url": "/portfolio-react-aleksandra/static/media/aleksandra-wolna.e4fdf3a7.jpg"
+  },
+  {
+    "revision": "75eb8f03cbe3bfb32d2f1a6ae266bb9c",
+    "url": "/portfolio-react-aleksandra/static/media/facebook.75eb8f03.svg"
+  },
+  {
+    "revision": "ffceac7b87a77d0b7b23c5b36b922cbd",
+    "url": "/portfolio-react-aleksandra/static/media/instagram.ffceac7b.svg"
+  },
+  {
+    "revision": "28c11c3e3aa0f496931ebf247aeea457",
+    "url": "/portfolio-react-aleksandra/static/media/pc.28c11c3e.svg"
+  },
+  {
+    "revision": "297d239db9d8cf013e598ba59719c2aa",
+    "url": "/portfolio-react-aleksandra/static/media/timeline.297d239d.svg"
+  },
+  {
+    "revision": "bd08eed18de79b787f52204799771a6a",
+    "url": "/portfolio-react-aleksandra/static/media/twitter.bd08eed1.svg"
+  }
+]);
