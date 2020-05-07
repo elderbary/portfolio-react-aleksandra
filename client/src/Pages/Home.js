abd8d853111526770e1382121d1020d587690813
Adam Kwiatek <salioner.adam@gmail.com>
2020-05-07 22:14:03 +0200

    fixes

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -13,34 +13,16 @@ import Picture from "../Components/Picture";
 import Social from "../Components/Social";
 
 import pc from '../images/pc.svg';
+import HomeItem from "../Components/HomeItem";
 
-class Test extends Component {
+class Home extends Component {
     render() {
         return (
             <div className="div-main">
                 <Container className="container-std">
                     <Row className="row-main">
                         <Col className="col-std col-toogle" md={1}>{<Toggle />}</Col>
-                        <Col className="col-std col-left" md={5}>
-                            <div className="div-left">
-                                <Container className="container-left">
-                                    <Row className="row-text">
-                                        <Col className="col-std col-text" md={12}><Text title="Tu będzie tytuł" text ="Mam 26 lat a środowisko, które mnie otacza, stanowią puste kubki po herbacie oraz sterty (nie)przeczytanych książek, od ciężaru których uginają się już półki. Piszę o książkach i serialach od 6 lat, od momentu w którym stwierdziłam, że to już czas. Czytam kryminały (szwedzkie), reportaże (wydawnictwa Czarnego) oraz obyczajówki (ciekawe). "/></Col>
-                                    </Row>
-                                    <Row className="row-stats">
-                                        <Col className="col-std col-stats"><Statistics score="3,456" description ="słów"/></Col>
-                                        <Col className="col-std col-stats"><Statistics score="12,123" description ="wyświetleń"/></Col>
-                                        <Col className="col-std col-stats"><Statistics score="" description =""/></Col>
-                                        <Col className="col-std col-stats"><Statistics score="" description =""/></Col>
-                                    </Row>
-                                </Container>
-                            </div>
-                        </Col>
-                        <Col className="col-std col-right" md={5}>
-                            <div className="div-right">
-                            <Picture imgportfolio={pc} alt="pc" />
-                            </div>
-                        </Col>
+                        <Col className="col-std col-inner" md={10}>{<HomeItem/>}</Col>
                         <Col className="col-std col-nav" md={1}>
                             {<Navigation />}
                             {<Social />}
@@ -55,4 +37,4 @@ class Test extends Component {
     }
 }
 
-export default Test;
+export default Home;
