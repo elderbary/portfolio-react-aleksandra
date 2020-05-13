b7c1dd2d380bdf066660d21e263489c491d6e709
Dawid Rozanski <drozanski@outlook.com>
2020-05-13 22:15:01 +0200

    .

--- client/src/Components/Item.js
+++ client/src/Components/Item.js
@@ -1,9 +1,5 @@
 import styled from "styled-components";
 
 export default styled.div`
-  height: 70vh;
-  width: 100%;
-  background-color: red;
-  color: #fff;
-  margin: 0 15px;
+
 `;
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -86,10 +86,12 @@ class Home extends Component {
                         <Col className="col-std col-left" md={5}>
                             <div className="div-left">
                                 <Container className="container-left">
+                                    <div className="div-carousel">
                                 <Carousel itemsToShow={1} itemsToScroll={1} enableSwipe={true} verticalMode={true} showArrows={false} pagination={false} ref={ref => (this.carousel = ref)}>
-                                    <Item> <Col className="col-std col-text"><Text title="Cześć, jestem Ola" text ="Mam 26 lat a środowisko, które mnie otacza, stanowią puste kubki po herbacie oraz sterty (nie)przeczytanych książek, od ciężaru których uginają się już półki. Piszę o książkach i serialach od 6 lat, od momentu w którym stwierdziłam, że to już czas. Czytam kryminały (szwedzkie), reportaże (wydawnictwa Czarnego) oraz obyczajówki (ciekawe). "/></Col> </Item>
+                                    <Item className="item-1"> <Col className="col-std col-text"><Text title="Cześć, jestem Ola" text ="Mam 26 lat a środowisko, które mnie otacza, stanowią puste kubki po herbacie oraz sterty (nie)przeczytanych książek, od ciężaru których uginają się już półki. Piszę o książkach i serialach od 6 lat, od momentu w którym stwierdziłam, że to już czas. Czytam kryminały (szwedzkie), reportaże (wydawnictwa Czarnego) oraz obyczajówki (ciekawe). "/></Col> </Item>
                                     <Item></Item>
                                 </Carousel>
+                                </div>
 <Row className="row-text">
                                        
                                     </Row>
