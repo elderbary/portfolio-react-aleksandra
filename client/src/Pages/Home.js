42d5db3b9b8f0b60a3c5a28d3dddab6339a332e7
Dawid Rozanski <drozanski@outlook.com>
2020-05-13 22:22:44 +0200

    ad

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -86,17 +86,16 @@ class Home extends Component {
                         <Col className="col-std col-left" md={5}>
                             <div className="div-left">
                                 <Container className="container-left">
-                                    <div className="div-carousel">
                                 <Carousel itemsToShow={1} itemsToScroll={1} enableSwipe={true} verticalMode={true} showArrows={false} pagination={false} ref={ref => (this.carousel = ref)}>
                                     <Item className="item-1"> <Col className="col-std col-text"><Text title="Cześć, jestem Ola" text ="Mam 26 lat a środowisko, które mnie otacza, stanowią puste kubki po herbacie oraz sterty (nie)przeczytanych książek, od ciężaru których uginają się już półki. Piszę o książkach i serialach od 6 lat, od momentu w którym stwierdziłam, że to już czas. Czytam kryminały (szwedzkie), reportaże (wydawnictwa Czarnego) oraz obyczajówki (ciekawe). "/></Col> </Item>
                                     <Item></Item>
                                 </Carousel>
-                                </div>
+                                
 <Row className="row-text">
                                        
                                     </Row>
                                     <Carousel itemsToShow={1} itemsToScroll={1} enableSwipe={true} verticalMode={true} showArrows={false} pagination={false} ref={ref => (this.carousel = ref)}>
-    <Item> <Row className="row-stats">
+            <Item> <Row className="row-stats">
                                         <Col className="col-std col-stats"><Statistics score="5 lat" description ="doświadczenia"/></Col>
                                         <Col className="col-std col-stats"><Statistics score="42" description ="klientów"/></Col>
                                         <Col className="col-std col-stats"><Statistics score="215" description ="artykułów"/></Col>
